3cfbda80ad8fa1807208a650ce8c3bb34f198c58
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-04-22 18:51:55 +0200

    Fix and add. Fix the logic for displaying the books in the grid by nesting the creation of the read-button in the forEach method of the button click event. Tested and commented out. Add the dialog tag for the future form for enter new books. Style the dialog acording to the rest of the web.

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 const myLibrary = [];
 const booksGrid = document.querySelector(".books-grid")
 
-const readButton = document.createElement('button')
-readButton.classList.add('secondary-button', 'read')
-readButton.textContent = "Read"
 
 
 
 function Book(title, author, pages) {
     this.title = title;
-    this. author = author;
+    this.author = author;
     this.pages = pages;
     this.read = true;
 
@@ -22,32 +19,41 @@ Book.prototype.toString = function bookToString () {
 
 const newButton = document.querySelector(".new")
 newButton.addEventListener('click', () => {
-    const newBook = new Book('Robin Hood', 'John Smith', 245)
-    myLibrary.push(newBook)
-    alert(newBook.toString())
-    console.table(myLibrary)
 
-    myLibrary.forEach((item) => {
-        const card = document.createElement('article')
-        card.classList.add("card")
+    const dialog = document.querySelector('dialog')
+    dialog.showModal()
+    // booksGrid.innerHTML = ""
+    // const newBook = new Book('Robin Hood', 'John Smith', 245)
+    // myLibrary.push(newBook)
+    // alert(newBook.toString())
+    // console.table(myLibrary)
 
-
-    
-        const title = document.createElement("p")
-        const author = document.createElement("p")
-        const pages = document.createElement("p")
-    
-        title.textContent = item.title
-        author.textContent = item.author
-        pages.textContent = item.pages
-    
-        card.appendChild(title)
-        card.appendChild(author)
-        card.appendChild(pages)
-        card.appendChild(readButton)
+    // myLibrary.forEach((item) => {
+    //     const card = document.createElement('article')
+    //     card.classList.add("card")
 
 
     
-        booksGrid.appendChild(card)
-    })
+    //     const title = document.createElement("p")
+    //     const author = document.createElement("p")
+    //     const pages = document.createElement("p")
+    //     const readButton = document.createElement('button')
+    
+    //     title.textContent = item.title
+    //     author.textContent = item.author
+    //     pages.textContent = item.pages
+    
+    //     card.appendChild(title)
+    //     card.appendChild(author)
+    //     card.appendChild(pages)
+    //     card.appendChild(readButton)
+
+    //     readButton.classList.add('secondary-button', 'read')
+    //     readButton.textContent = "Read"
+
+
+
+    
+    //     booksGrid.appendChild(card)
+    // })
 })
