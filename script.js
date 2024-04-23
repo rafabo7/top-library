32fe8942235d9e9902b5ee771ee454d7bf1981f4
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-04-23 19:33:45 +0200

    Add the logic for new books Add the logic for new books already tested with the mock in the actual submit button. The logic catch the users input and pass it the to Book constructor. The resulting object is pushed to myLibrary array. The with an iterator myLibrary is displayed with its items as card in the books-grid section. Cons: Performance. Everytime a book is entered the books-grid gets cleared, then populated again with the objects of the array. It seems to work.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const myLibrary = [];
 const booksGrid = document.querySelector(".books-grid")
 
+//Targeting the inner "+ Add" button, inside the form.
+const addButton = document.querySelector('#add-button')
+
 
 
 
@@ -22,38 +25,53 @@ newButton.addEventListener('click', () => {
 
     const dialog = document.querySelector('dialog')
     dialog.showModal()
-    // booksGrid.innerHTML = ""
-    // const newBook = new Book('Robin Hood', 'John Smith', 245)
-    // myLibrary.push(newBook)
-    // alert(newBook.toString())
-    // console.table(myLibrary)
+    
+})
 
-    // myLibrary.forEach((item) => {
-    //     const card = document.createElement('article')
-    //     card.classList.add("card")
+addButton.addEventListener('click', () => {
+    booksGrid.innerHTML = ""
+
+    const title = document.querySelector('#title')
+    const author = document.querySelector('#author')
+    const pages = document.querySelector('#pages')
+
+    const newBook = new Book(title.value, author.value, pages.value)
+    
+    title.value = ""
+    author.value = ""
+    pages.value = ""
+
+    myLibrary.push(newBook)
+
+    myLibrary.forEach((item) => {
+        const card = document.createElement('article')
+        card.classList.add("card")
 
 
     
-    //     const title = document.createElement("p")
-    //     const author = document.createElement("p")
-    //     const pages = document.createElement("p")
-    //     const readButton = document.createElement('button')
+        const title = document.createElement("p")
+        const author = document.createElement("p")
+        const pages = document.createElement("p")
+        const readButton = document.createElement('button')
     
-    //     title.textContent = item.title
-    //     author.textContent = item.author
-    //     pages.textContent = item.pages
+        title.textContent = item.title
+        author.textContent = item.author
+        pages.textContent = item.pages
     
-    //     card.appendChild(title)
-    //     card.appendChild(author)
-    //     card.appendChild(pages)
-    //     card.appendChild(readButton)
+        card.appendChild(title)
+        card.appendChild(author)
+        card.appendChild(pages)
+        card.appendChild(readButton)
 
-    //     readButton.classList.add('secondary-button', 'read')
-    //     readButton.textContent = "Read"
+        readButton.classList.add('secondary-button', 'read')
+        readButton.textContent = "Read"
 
+        booksGrid.appendChild(card)
 
+    })
 
     
-    //     booksGrid.appendChild(card)
-    // })
+
+    
+    
 })
