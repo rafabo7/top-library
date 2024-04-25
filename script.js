4c171338aba342cd771921e742b52ec79e68521c
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-04-25 18:11:16 +0200

    Add the logic for removing a book. Add the logic for removing a book, the tricky part: both from the screen and the data.

--- script.js
+++ script.js
@@ -1,58 +1,76 @@
-const myLibrary = [];
-const booksGrid = document.querySelector(".books-grid")
+//Storage
+let myLibrary = [];
 
-//Targeting the inner "+ Add" button, inside the form.
-const addButton = document.querySelector('#add-button')
-
-
-
-
+//Book object constructor
 function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = true;
-
-    
+    this.read = true;    
 }
 
 Book.prototype.toString = function bookToString () {
     return `${this.title} by ${this.author}`
 }
 
+//User interface
+const booksGrid = document.querySelector(".books-grid")
 const newButton = document.querySelector(".new")
-newButton.addEventListener('click', () => {
+const newBookForm = document.querySelector('#new-book-form')
+const addButton = document.querySelector('#add-button')
+const dialog = document.querySelector('dialog')
 
-    const dialog = document.querySelector('dialog')
-    dialog.showModal()
-    
-})
+//Functions
+function showDialog () {dialog.showModal()}
 
-addButton.addEventListener('click', () => {
-    booksGrid.innerHTML = ""
+function createBook(){
+    const title = document.querySelector('#title').value
+    const author = document.querySelector('#author').value
+    const pages = document.querySelector('#pages').value
 
-    const title = document.querySelector('#title')
-    const author = document.querySelector('#author')
-    const pages = document.querySelector('#pages')
+    return new Book(title, author, pages)
+}
 
-    const newBook = new Book(title.value, author.value, pages.value)
-    
-    title.value = ""
-    author.value = ""
-    pages.value = ""
-
-    myLibrary.push(newBook)
-
-    myLibrary.forEach((item) => {
-        const card = document.createElement('article')
-        card.classList.add("card")
+function createCard(item){
+    const card = document.createElement('article')
+    card.classList.add("card")
 
 
-    
-        const title = document.createElement("p")
-        const author = document.createElement("p")
-        const pages = document.createElement("p")
-        const readButton = document.createElement('button')
+
+    const title = document.createElement("p")
+    const author = document.createElement("p")
+    const pages = document.createElement("p")
+    const readButton = document.createElement('button')
+    const deleteButton = document.createElement('button')
+
+    title.classList.add('card-title')
+    author.classList.add('card-author')
+    pages.classList.add('card-pages')
+
+    readButton.classList.add('secondary-button', 'read')
+    readButton.textContent = "Read"
+
+    deleteButton.classList.add('red-button')
+    deleteButton.setAttribute('data-index', myLibrary.indexOf(item))
+    deleteButton.textContent = 'Delete'
+
+    deleteButton.addEventListener('click', (e) => {
+        //Target the elemente
+        let target = e.target
+        //Get the index of the element to remove
+        let index = target.getAttribute('data-index')
+        console.log(index)
+
+        //Target the card of the element
+        let card = target.closest('.card')
+        
+        //Remove book card from UI
+        card.remove()
+
+        //Remove book data from array
+        myLibrary.splice(index, 1)
+
+    })
     
         title.textContent = item.title
         author.textContent = item.author
@@ -62,13 +80,35 @@ addButton.addEventListener('click', () => {
         card.appendChild(author)
         card.appendChild(pages)
         card.appendChild(readButton)
+        card.appendChild(deleteButton)
 
-        readButton.classList.add('secondary-button', 'read')
-        readButton.textContent = "Read"
 
         booksGrid.appendChild(card)
 
-    })
+    
+
+}
+
+function readStatus(){
+
+}
+
+//Events
+newButton.addEventListener('click', showDialog)
+
+
+addButton.addEventListener('click', () => {
+    booksGrid.innerHTML = ""
+    
+    const newBook = createBook()
+    
+    myLibrary.push(newBook)
+    
+    
+    newBookForm.reset()
+
+    myLibrary.forEach((item) => {createCard(item)})
+        
 
     
 
