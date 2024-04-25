031941dbda78af96befe5db3be24ca986bc56ee6
rafabo7 <122386430+rafabo7@users.noreply.github.com>
2024-04-25 20:08:37 +0200

    Add and fix. Restructured the logic for removing the book to its own function for better readability. Create a method in Book.prototype for toggle the read property between true and false. Create the logic for th readButton too, linked to mentioned method. Add a css class logic to change the background color fo the readButton.

--- script.js
+++ script.js
@@ -13,6 +13,15 @@ Book.prototype.toString = function bookToString () {
     return `${this.title} by ${this.author}`
 }
 
+Book.prototype.toggleRead = function readStatus() {
+    if (this.read === true) {
+        this.read = false
+    } else if (this.read === false) {
+        this.read = true
+    }
+
+}
+
 //User interface
 const booksGrid = document.querySelector(".books-grid")
 const newButton = document.querySelector(".new")
@@ -31,6 +40,44 @@ function createBook(){
     return new Book(title, author, pages)
 }
 
+function removeBook(e) {
+    //Target the elemente
+    let target = e.target
+    //Get the index of the element to remove
+    let index = target.getAttribute('data-index')
+    console.log(index)
+
+    //Target the card of the element
+    let card = target.closest('.card')
+    
+    //Remove book card from UI
+    card.remove()
+
+    //Remove book data from array
+    myLibrary.splice(index, 1)
+
+}
+
+function changeReadStatus (e) {
+    let target = e.target
+    let index = target.getAttribute('data-index')
+    let book = myLibrary[index]
+
+    book.toggleRead()
+
+    if (book.read === true) {
+        target.textContent = "Read"
+        target.classList.remove('not-read')
+        target.classList.add('read')
+    } else {
+        target.textContent = "Not Read"
+        target.classList.remove('read')
+        target.classList.add('not-read')
+
+    }
+
+}
+
 function createCard(item){
     const card = document.createElement('article')
     card.classList.add("card")
@@ -47,51 +94,32 @@ function createCard(item){
     author.classList.add('card-author')
     pages.classList.add('card-pages')
 
-    readButton.classList.add('secondary-button', 'read')
+    readButton.classList.add('secondary-button')
     readButton.textContent = "Read"
+    readButton.setAttribute('data-index', myLibrary.indexOf(item))
 
     deleteButton.classList.add('red-button')
     deleteButton.setAttribute('data-index', myLibrary.indexOf(item))
     deleteButton.textContent = 'Delete'
 
-    deleteButton.addEventListener('click', (e) => {
-        //Target the elemente
-        let target = e.target
-        //Get the index of the element to remove
-        let index = target.getAttribute('data-index')
-        console.log(index)
-
-        //Target the card of the element
-        let card = target.closest('.card')
-        
-        //Remove book card from UI
-        card.remove()
-
-        //Remove book data from array
-        myLibrary.splice(index, 1)
-
-    })
+    deleteButton.addEventListener('click', (e) => {removeBook(e)})
+    readButton.addEventListener('click', (e) => {changeReadStatus(e)})
     
-        title.textContent = item.title
-        author.textContent = item.author
-        pages.textContent = item.pages
-    
-        card.appendChild(title)
-        card.appendChild(author)
-        card.appendChild(pages)
-        card.appendChild(readButton)
-        card.appendChild(deleteButton)
+    title.textContent = item.title
+    author.textContent = item.author
+    pages.textContent = item.pages
+
+    card.appendChild(title)
+    card.appendChild(author)
+    card.appendChild(pages)
+    card.appendChild(readButton)
+    card.appendChild(deleteButton)
 
 
-        booksGrid.appendChild(card)
-
-    
+    booksGrid.appendChild(card) 
 
 }
-
-function readStatus(){
-
-}
+//End of the functions
 
 //Events
 newButton.addEventListener('click', showDialog)
@@ -108,10 +136,5 @@ addButton.addEventListener('click', () => {
     newBookForm.reset()
 
     myLibrary.forEach((item) => {createCard(item)})
-        
-
-    
-
-    
-    
+   
 })
